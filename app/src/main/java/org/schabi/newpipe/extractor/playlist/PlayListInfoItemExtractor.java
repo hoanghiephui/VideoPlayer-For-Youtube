298809f83043af6e3b4f7b70951f0239b4a5376c
Hoang Hiep <hoanghiep.hui@gmail.com>
2017-06-13 08:31:37 +0700

    add submodule

--- app/src/main/java/org/schabi/newpipe/extractor/playlist/PlayListInfoItemExtractor.java
+++
@@ -1,9 +0,0 @@
-package org.schabi.newpipe.extractor.playlist;
-
-import org.schabi.newpipe.extractor.exceptions.ParsingException;
-
-public interface PlayListInfoItemExtractor {
-    String getThumbnailUrl() throws ParsingException;
-    String getPlayListName() throws ParsingException;
-    String getWebPageUrl() throws ParsingException;
-}
